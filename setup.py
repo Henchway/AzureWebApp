cc4a53fd196d1ad0da8c7212e5e84e0cd75fd954
Thomas Scheibelhofer <thomas.scheibelhofer@live.at>
2023-09-18 12:41:42 +0200

    Init

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='AzureWebApp',
+    version='',
+    packages=['fastapi', 'uvicorn'],
+    url='',
+    license='',
+    author='ScheibeT',
+    author_email='',
+    description=''
+)
